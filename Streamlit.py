5abf3af408a48fa9dae7c92cbef9bea1c2b8edc1
RheedWhan <132687820+RheedWhan@users.noreply.github.com>
2024-01-01 23:09:28 +0100

    Created using Colaboratory

--- Streamlit.py
+++ Streamlit.py
@@ -1,21 +1,4 @@
 {
-  "nbformat": 4,
-  "nbformat_minor": 0,
-  "metadata": {
-    "colab": {
-      "provenance": [],
-      "mount_file_id": "1NZjfbsEu9kXQqqy9rAbgcVG6bWIY_90m",
-      "authorship_tag": "ABX9TyM/VnZ4+q4otPtTpEVk5G5g",
-      "include_colab_link": true
-    },
-    "kernelspec": {
-      "name": "python3",
-      "display_name": "Python 3"
-    },
-    "language_info": {
-      "name": "python"
-    }
-  },
   "cells": [
     {
       "cell_type": "markdown",
@@ -29,25 +12,25 @@
     },
     {
       "cell_type": "code",
-      "source": [
-        "%%writefile app.py"
-      ],
+      "execution_count": null,
       "metadata": {
         "colab": {
           "base_uri": "https://localhost:8080/"
         },
         "id": "h8WAIjH6_Du8",
-        "outputId": "acba1918-7c09-4209-c983-7ee97e5f8100"
+        "outputId": "78798361-e0bd-4861-a32a-a8d93ec266fd"
       },
-      "execution_count": 11,
       "outputs": [
         {
-          "output_type": "stream",
           "name": "stdout",
+          "output_type": "stream",
           "text": [
             "Writing app.py\n"
           ]
         }
+      ],
+      "source": [
+        "%%writefile app.py"
       ]
     },
     {
@@ -55,39 +38,49 @@
       "execution_count": null,
       "metadata": {
         "colab": {
+          "background_save": true,
           "base_uri": "https://localhost:8080/"
         },
         "id": "JFgjsVY12qKw",
-        "outputId": "62480f8d-1db5-478a-874f-6a0ace894747"
+        "outputId": "28e686ea-c0d0-40df-d9fb-1eb81dad4bbf"
       },
       "outputs": [
         {
-          "output_type": "stream",
           "name": "stdout",
+          "output_type": "stream",
           "text": [
-            "35.245.236.137\n",
-            "[##................] / fetchMetadata: sill resolveWithNewModule ms@2.1.2 checki\u001b[0m\u001b[K\n",
+            "\u001b[2K     \u001b[90m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\u001b[0m \u001b[32m8.4/8.4 MB\u001b[0m \u001b[31m22.7 MB/s\u001b[0m eta \u001b[36m0:00:00\u001b[0m\n",
+            "\u001b[2K     \u001b[90m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\u001b[0m \u001b[32m190.6/190.6 kB\u001b[0m \u001b[31m13.3 MB/s\u001b[0m eta \u001b[36m0:00:00\u001b[0m\n",
+            "\u001b[2K     \u001b[90m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\u001b[0m \u001b[32m4.8/4.8 MB\u001b[0m \u001b[31m40.4 MB/s\u001b[0m eta \u001b[36m0:00:00\u001b[0m\n",
+            "\u001b[2K     \u001b[90m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\u001b[0m \u001b[32m82.1/82.1 kB\u001b[0m \u001b[31m7.9 MB/s\u001b[0m eta \u001b[36m0:00:00\u001b[0m\n",
+            "\u001b[2K     \u001b[90m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\u001b[0m \u001b[32m62.7/62.7 kB\u001b[0m \u001b[31m6.2 MB/s\u001b[0m eta \u001b[36m0:00:00\u001b[0m\n",
+            "\u001b[?25h34.27.8.65\n",
+            "\n",
             "Collecting usage statistics. To deactivate, set browser.gatherUsageStats to False.\n",
             "\u001b[0m\n",
             "\u001b[0m\n",
             "\u001b[34m\u001b[1m  You can now view your Streamlit app in your browser.\u001b[0m\n",
             "\u001b[0m\n",
             "\u001b[34m  Network URL: \u001b[0m\u001b[1mhttp://172.28.0.12:8501\u001b[0m\n",
-            "\u001b[34m  External URL: \u001b[0m\u001b[1mhttp://35.245.236.137:8501\u001b[0m\n",
-            "\u001b[0m\n",
-            "\u001b[K\u001b[?25hnpx: installed 22 in 6.999s\n",
-            "your url is: https://loud-impalas-eat.loca.lt\n"
+            "\u001b[34m  External URL: \u001b[0m\u001b[1mhttp://34.27.8.65:8501\u001b[0m\n",
+            "\u001b[0m\n"
           ]
         }
       ],
       "source": [
         "! pip install streamlit -q\n",
-        "!wget -q -O - ipv4.icanhazip.com\n",
-        "! streamlit run app.py & npx localtunnel --port 8501"
+        "! pip install --upgrade streamlit -q\n",
+        "! wget -q -O - ipv4.icanhazip.com\n",
+        "! streamlit run app.py --server.port 8501"
       ]
     },
     {
       "cell_type": "code",
+      "execution_count": null,
+      "metadata": {
+        "id": "FqnEGiRz3crk"
+      },
+      "outputs": [],
       "source": [
         "import streamlit as st\n",
         "st.write('# Streamlit calculator')\n",
@@ -95,21 +88,33 @@
         "number2 = st.number_input('number 2')\n",
         "num3 = number1+number2\n",
         "st.write('# Answer is ',num3)"
-      ],
-      "metadata": {
-        "id": "FqnEGiRz3crk"
-      },
-      "execution_count": 10,
-      "outputs": []
+      ]
     },
     {
       "cell_type": "code",
-      "source": [],
+      "execution_count": null,
       "metadata": {
         "id": "nRvpBtb_6bJh"
       },
-      "execution_count": null,
-      "outputs": []
+      "outputs": [],
+      "source": []
     }
-  ]
+  ],
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "mount_file_id": "1NZjfbsEu9kXQqqy9rAbgcVG6bWIY_90m",
+      "authorship_tag": "ABX9TyPADwB+kmT+z+H2LzzgGQBV",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "display_name": "Python 3",
+      "name": "python3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "nbformat": 4,
+  "nbformat_minor": 0
 }
